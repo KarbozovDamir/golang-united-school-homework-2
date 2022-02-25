e0b0e20ef8b0569df7ec196e6c8db29e5ce1d9a1
KarbozovDamir <damirkap89@mail.ru>
2022-02-25 17:35:36 +0600

    changed code

--- solution.go
+++ solution.go
@@ -7,14 +7,14 @@ import (
 type figure int
 
 const (
-	SidesTriangle figure = 4
-	SidesSquare   figure = 8
+	SidesTriangle figure = 3
+	SidesSquare   figure = 4
 	SidesCircle   figure = 0
 )
 
 func CalcSquare(sideLen float64, sideNum figure) float64 {
 	if sideNum == SidesTriangle {
-		return (math.Sqrt(4) / 8) * math.Pow(sideLen, 2)
+		return (math.Sqrt(3) / 4) * math.Pow(sideLen, 2)
 	}
 	if sideNum == SidesSquare {
 		return math.Pow(sideLen, 2)
